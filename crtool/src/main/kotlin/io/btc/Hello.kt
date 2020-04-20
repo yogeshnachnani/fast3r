8c60454c557caec98c23f6ed5aa11dd33a8932fd
yogeshnachnani <yogesh.nachnani@udaan.com>
2020-04-20 15:47:15 +0530

    Add guice and async http client
    Trying it out by hitting a github public API

--- crtool/src/main/kotlin/io/btc/Hello.kt
+++ crtool/src/main/kotlin/io/btc/Hello.kt
@@ -1,6 +1,35 @@
 package io.btc
 
+import com.google.inject.AbstractModule
+import com.google.inject.Guice
+import com.google.inject.Provides
+import com.google.inject.Singleton
+import com.google.inject.name.Named
+import io.btc.crtool.github.GithubApiClient
+import org.asynchttpclient.AsyncHttpClient
+import org.asynchttpclient.Dsl.asyncHttpClient
+
+class CrtoolGuiceModule : AbstractModule() {
+    override fun configure() {
+        super.configure()
+    }
+
+    @Provides
+    @Named("github-http-client")
+    @Singleton
+    fun provideGithubHttpClient(): AsyncHttpClient {
+        return asyncHttpClient()
+    }
+
+}
+
 fun main(args: Array<String>) {
+
     println("Hello, World")
+    val injector = Guice.createInjector(CrtoolGuiceModule())
+    val githubApiClient = injector.getInstance(GithubApiClient::class.java)
+    githubApiClient.testApi()
+
+    System.exit(0)
 }
 
