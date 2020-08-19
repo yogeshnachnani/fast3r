ab49e205de37e77425795b7acf0190b160532c80
yogeshnachnani <yogesh@theboringtech.io>
2020-08-19 17:24:28 +0530

    Introduce multiple fixes for electron app launch
    * Used execFile to launch the backend server process
    * Passing a 'resourcePath' as a property to the java project which
      points to where the static content is stored when the app is shipped
      via electron

--- src/jvmMain/kotlin/io/btc/supercr/api/StaticApi.kt
+++ src/jvmMain/kotlin/io/btc/supercr/api/StaticApi.kt
@@ -3,19 +3,26 @@ package io.btc.supercr.api
 import io.ktor.http.content.default
 import io.ktor.http.content.files
 import io.ktor.http.content.static
+import io.ktor.http.content.staticRootFolder
 import io.ktor.routing.Routing
-import io.ktor.routing.route
+import io.ktor.routing.application
 import org.slf4j.LoggerFactory
+import java.io.File
 
-class StaticApi constructor(
+class StaticApi(
     routing: Routing
 ) : ApiController(routing) {
     companion object {
         private val logger = LoggerFactory.getLogger(StaticApi::class.java)
     }
     override fun initRoutes(routing: Routing) {
+        val customResourcePath = System.getProperty("resourcePath")
         routing {
             static("/static")  {
+                if (!customResourcePath.isNullOrEmpty()) {
+                    staticRootFolder = File("$customResourcePath/lib")
+                    logger.info("Serving from {}", staticRootFolder?.absolutePath)
+                }
                 files("js")
                 default("index.html")
             }
