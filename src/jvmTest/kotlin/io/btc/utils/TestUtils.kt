2b136413be2cfe59a87fc5af818a8429cbd7dd54
yogeshnachnani <yogesh@theboringtech.io>
2021-04-30 21:55:14 +0530

    Fix build
    Also add a helpful comment so future devs don't feel like shooting me

--- src/jvmTest/kotlin/io/btc/utils/TestUtils.kt
+++ src/jvmTest/kotlin/io/btc/utils/TestUtils.kt
@@ -13,7 +13,8 @@ class TestUtils {
     companion object {
         val btcRepoDir: String =  TestUtils::class.java.classLoader.getResource("placeholder")!!.file
             .let {
-                File(it).parentFile.parentFile.parentFile.parentFile.parentFile.parentFile.absolutePath
+                /** We are in : src/jvmTest/resources/placeholder */
+                File(it).parentFile.parentFile.parentFile.parentFile.parentFile.absolutePath
             }
         const val validBtcRef = "ae6adbf9f142c8591e2128484c87c4e50cdc19e7"
 
